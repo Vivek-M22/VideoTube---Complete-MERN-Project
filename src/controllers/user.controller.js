99cb490719167642fd39600d034c9b3371eded86
Vivek Makwana <vivekmakwana.one@gmail.com>
2024-12-29 18:33:49 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import {asyncHandler} from "../utils/asyncHandletr.js";
+import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import {User} from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/Cloudinary.js";
@@ -19,28 +19,34 @@ const registerUser = asyncHandler( async (req , res) => {
 
     // 1. get user details from frontend
     const {fullName , email , username , password} = req.body
-    console.log("email : " , email);
+    // console.log("email : " , email);
 
     // 2. validation -- not empty
     if(
-        [fullName , email , username , password ].some((field) => 
-        field?.trim() === "")
+        [fullName , email , username , password ].some((field) => field?.trim() === "")
     ) {
         throw new ApiError(400 , "All fields are required")
     }
 
     // 3. check if user already exist : username , email
-    const existedUser = User.findOne({
-        $or : [{email} , {username} ]
+    const existedUser = await User.findOne({
+        $or : [{ username } , { email } ]
     })
 
     if(existedUser){
         throw new ApiError(409 , "User with email or username already exists")
     }
+    // console.log(req.files);
 
     // 4. for new register : check for image and avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && 
+    req.files.coverImage.length > 0 ){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400 , "Avatar file is required")
@@ -55,13 +61,13 @@ const registerUser = asyncHandler( async (req , res) => {
     }
 
     // 6. create user object - create entry in db
-    const user = User.create({
+    const user = await User.create({
         fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
         password,
-        username: username.toLowercase()
+        username: username.toLowerCase()
     })
 
     // 7. remove password and refresh token field from response 
